1455b46d62bd50767d995e79be902b70e5731a3a
eman obeidat <amoonobeidat43@gmail.com>
2023-05-07 14:33:23 +0300

    edit

--- math-series/mathSeries/series.py
+++ math-series/mathSeries/series.py
@@ -1,25 +1,36 @@
 def fibonacci(n):
-    if n == 0:
+     '''  This function calculate the Fibonacci for integer
+        parameter n and return the result
+        
+     '''
+     if n == 0:
         return 0
-    elif n == 1:
+     elif n == 1:
         return 1
-    else:
+     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
-    if n == 0:
+     '''  This function calculate the Lacus for integer
+        parameter n and return the result
+        
+     '''
+     if n == 0:
         return 2
-    elif n == 1:
+     elif n == 1:
         return 1
-    else:
+     else:
         return lucas(n-1) + lucas(n-2)
 
 def sum_series(n, first, second):
-        if(first==0 and second==1):
+         '''  This function calculate the summation series for integer
+             parameter n / first / second and return the result
+                '''
+         if(first==0 and second==1):
            return fibonacci(n)
-        elif (first==2 and second==1):
+         elif (first==2 and second==1):
             return lucas(n)
-        else:
+         else:
            return sum_series(n-1, first, second) + sum_series(n-2, first, second)
         
 
